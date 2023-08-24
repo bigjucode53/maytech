ec2962d03c66a2c2ad322188aaaee6d9706531e7
Julien Le Tiec <jletiec@gmail.com>
2023-08-24 12:18:21 +0200

    orange

--- js/sso.js
+++ js/sso.js
@@ -11,157 +11,157 @@ SmoothScroll({
     touchpadSupport: true,
   })
   
-  let data
-  const iframe = document.createElement('iframe')
-  iframe.id = 'inlineFrameExample'
-  iframe.width = 0
-  iframe.height = 0
-  iframe.style.display = 'none'
-  iframe.src = 'https://platform.May'Tech.us'
-  document.body.appendChild(iframe)
+  // let data
+  // const iframe = document.createElement('iframe')
+  // iframe.id = 'inlineFrameExample'
+  // iframe.width = 0
+  // iframe.height = 0
+  // iframe.style.display = 'none'
+  // // iframe.src = 'https://platform.May'Tech.us'
+  // document.body.appendChild(iframe)
   
-  const CrossOriginLocalStorage = function (currentWindow, iframe, onMessage) {
-    let childWindow
-    try {
-      childWindow = iframe.contentWindow
-    } catch (e) {
-      childWindow = iframe.contentWindow
-    }
+  // const CrossOriginLocalStorage = function (currentWindow, iframe, onMessage) {
+  //   let childWindow
+  //   try {
+  //     childWindow = iframe.contentWindow
+  //   } catch (e) {
+  //     childWindow = iframe.contentWindow
+  //   }
   
-    currentWindow.onmessage = (event) => {
-      if (typeof event.data !== 'string') {
-        return
-      }
-      return onMessage(JSON.parse(event.data))
-    }
+  //   currentWindow.onmessage = (event) => {
+  //     if (typeof event.data !== 'string') {
+  //       return
+  //     }
+  //     return onMessage(JSON.parse(event.data))
+  //   }
   
-    this.getData = (key) => {
-      const messageData = {
-        key: key,
-        method: 'get',
-      }
-      this.postMessage(messageData)
-    }
+  //   this.getData = (key) => {
+  //     const messageData = {
+  //       key: key,
+  //       method: 'get',
+  //     }
+  //     this.postMessage(messageData)
+  //   }
   
-    this.setData = (key, data) => {
-      const messageData = {
-        key: key,
-        method: 'set',
-        data: data,
-      }
-      this.postMessage(messageData)
-    }
+  //   this.setData = (key, data) => {
+  //     const messageData = {
+  //       key: key,
+  //       method: 'set',
+  //       data: data,
+  //     }
+  //     this.postMessage(messageData)
+  //   }
   
-    this.removeData = (key) => {
-      const messageData = {
-        key: key,
-        method: 'remove',
-      }
-      this.postMessage(messageData)
-    }
+  //   this.removeData = (key) => {
+  //     const messageData = {
+  //       key: key,
+  //       method: 'remove',
+  //     }
+  //     this.postMessage(messageData)
+  //   }
   
-    this.postMessage = (messageData) => {
-      childWindow.postMessage(JSON.stringify(messageData), '*')
-    }
-  }
+  //   this.postMessage = (messageData) => {
+  //     childWindow.postMessage(JSON.stringify(messageData), '*')
+  //   }
+  // }
   
-  let cross
+  // let cross
   
-  window.onload = () => {
-    const onMessage = (payload) => {
-      switch (payload.method) {
-        case 'get':
-          data = payload
-          if (document.readyState != 'complete') {
-            document.addEventListener('DOMContentLoaded', appendAccount)
-          } else {
-            if (!payload['data']) {
-              return
-            }
-            appendAccount()
-          }
-          break
-        default:
-          console.error('Unknown method "' + payload.method + '"', payload)
-      }
-    }
+  // window.onload = () => {
+  //   const onMessage = (payload) => {
+  //     switch (payload.method) {
+  //       case 'get':
+  //         data = payload
+  //         if (document.readyState != 'complete') {
+  //           document.addEventListener('DOMContentLoaded', appendAccount)
+  //         } else {
+  //           if (!payload['data']) {
+  //             return
+  //           }
+  //           appendAccount()
+  //         }
+  //         break
+  //       default:
+  //         console.error('Unknown method "' + payload.method + '"', payload)
+  //     }
+  //   }
   
-    const iframe = document.getElementById('inlineFrameExample')
-    cross = new CrossOriginLocalStorage(window, iframe, onMessage)
-    cross.getData('token')
+  //   const iframe = document.getElementById('inlineFrameExample')
+  //   cross = new CrossOriginLocalStorage(window, iframe, onMessage)
+  //   cross.getData('token')
   
-    window.addEventListener('focus', function () {
-      cross.getData('token')
-    })
-  }
+  //   window.addEventListener('focus', function () {
+  //     cross.getData('token')
+  //   })
+  // }
   
-  const removeToken = (e) => {
-    cross.removeData('token')
-    window.open('https://platform.May'Tech.us/signin', '_self')
-  }
+  // const removeToken = (e) => {
+  //   cross.removeData('token')
+  //   window.open('https://platform.May'Tech.us/signin', '_self')
+  // }
   
-  const appendAccount = () => {
+  // const appendAccount = () => {
   
-    document.querySelectorAll('.sign-up').forEach(i => i.remove())
-    document.querySelector('.hero-btn-wrapper').remove()
+  //   document.querySelectorAll('.sign-up').forEach(i => i.remove())
+  //   document.querySelector('.hero-btn-wrapper').remove()
   
-    document.querySelectorAll('.log-in').forEach(i => {
-      i.querySelector('.subtitle').innerHTML = 'Account'
-      i.classList.add('account-btn')
-      i.href = 'https://platform.May'Tech.us/account'
-    })
+  //   document.querySelectorAll('.log-in').forEach(i => {
+  //     i.querySelector('.subtitle').innerHTML = 'Account'
+  //     i.classList.add('account-btn')
+  //     i.href = 'https://platform.May'Tech.us/account'
+  //   })
   
-    const menus = document.querySelectorAll('.menu-btn-items')
-    const accountBtnWrapper = document.createElement('div')
-    accountBtnWrapper.classList.add('account-btn-wrapper')
+  //   const menus = document.querySelectorAll('.menu-btn-items')
+  //   const accountBtnWrapper = document.createElement('div')
+  //   accountBtnWrapper.classList.add('account-btn-wrapper')
   
-    const mobileMenuBtn = document.createElement('div')
-    mobileMenuBtn.classList.add('account-btn-wrapper')
-    mobileMenuBtn.style.flexGrow = 1
-    mobileMenuBtn.innerHTML = `
-         <a href="https://platform.May'Tech.us/account" target="_self" class="account-btn btn flex-hor w-inline-block" style="justify-content: center">
-           <img src="images/Profile-white.svg" loading="lazy" alt="" class="profile-icon">
-           <div class="subtitle white-text">Account</div>
-         </a>
-       `
+  //   const mobileMenuBtn = document.createElement('div')
+  //   mobileMenuBtn.classList.add('account-btn-wrapper')
+  //   mobileMenuBtn.style.flexGrow = 1
+  //   mobileMenuBtn.innerHTML = `
+  //        <a href="https://platform.May'Tech.us/account" target="_self" class="account-btn btn flex-hor w-inline-block" style="justify-content: center">
+  //          <img src="images/Profile-white.svg" loading="lazy" alt="" class="profile-icon">
+  //          <div class="subtitle white-text">Account</div>
+  //        </a>
+  //      `
   
-    menus.forEach(i => {
-      const btn = i.innerHTML
-      i.innerHTML = ''
-      i.classList.add('account-btn-position')
-      accountBtnWrapper.innerHTML = `
-  ${btn}
-         <ul class="account-btn-sublist">
-           <li class="account-btn-sublist-item">
-             <div class="account-btn-sublist-name">
-               <div class="account-btn-sublist-name-icon">${data?.username
-        ? data.username[0]
-        : ''}</div>
-               <div>${data?.username || 'Account'}</div>
-             </div>
-           </li>
-           <li class="account-btn-sublist-item">
-             <a href="https://platform.May'Tech.us/requests"><img src="images/account-btn-req.svg" loading="lazy" alt="" class="profile-icon">Requests</a>
-           </li>
-           <li class="account-btn-sublist-item">
-             <a href="https://platform.May'Tech.us/companies"><img src="images/account-btn-companies.svg" loading="lazy" alt="" class="profile-icon">Companies</a>
-           </li>
-           <li class="account-btn-sublist-item">
-             <a href="https://platform.May'Tech.us/account"><img src="images/account-btn-personal.svg" loading="lazy" alt="" class="profile-icon">Personal Area</a>
-           </li>
-           <li class="account-btn-sublist-item">
-             <a href="https://platform.May'Tech.us/developers"><img src="images/account-btn-bench.svg" loading="lazy" alt="" class="profile-icon">Bench</a>
-           </li>
-           <li class="account-btn-sublist-item">
-             <a href="https://platform.May'Tech.us/chat"><img src="images/account-btn-chat.svg" loading="lazy" alt="" class="profile-icon">Chat</a>
-           </li>
-           <li class="account-btn-sublist-item">
-             <a href="#" onclick="removeToken()"><img src="images/account-btn-logout.svg" loading="lazy" alt="" class="profile-icon">Log out</a>
-           </li>
-         </ul>
-       `
-      i.appendChild(accountBtnWrapper.cloneNode(true))
-    })
-    document.querySelector('.menu-btn-wrapper').appendChild(mobileMenuBtn)
-  }
+  //   menus.forEach(i => {
+  //     const btn = i.innerHTML
+  //     i.innerHTML = ''
+  //     i.classList.add('account-btn-position')
+  //     accountBtnWrapper.innerHTML = `
+  // ${btn}
+  //        <ul class="account-btn-sublist">
+  //          <li class="account-btn-sublist-item">
+  //            <div class="account-btn-sublist-name">
+  //              <div class="account-btn-sublist-name-icon">${data?.username
+  //       ? data.username[0]
+  //       : ''}</div>
+  //              <div>${data?.username || 'Account'}</div>
+  //            </div>
+  //          </li>
+  //          <li class="account-btn-sublist-item">
+  //            <a href="https://platform.May'Tech.us/requests"><img src="images/account-btn-req.svg" loading="lazy" alt="" class="profile-icon">Requests</a>
+  //          </li>
+  //          <li class="account-btn-sublist-item">
+  //            <a href="https://platform.May'Tech.us/companies"><img src="images/account-btn-companies.svg" loading="lazy" alt="" class="profile-icon">Companies</a>
+  //          </li>
+  //          <li class="account-btn-sublist-item">
+  //            <a href="https://platform.May'Tech.us/account"><img src="images/account-btn-personal.svg" loading="lazy" alt="" class="profile-icon">Personal Area</a>
+  //          </li>
+  //          <li class="account-btn-sublist-item">
+  //            <a href="https://platform.May'Tech.us/developers"><img src="images/account-btn-bench.svg" loading="lazy" alt="" class="profile-icon">Bench</a>
+  //          </li>
+  //          <li class="account-btn-sublist-item">
+  //            <a href="https://platform.May'Tech.us/chat"><img src="images/account-btn-chat.svg" loading="lazy" alt="" class="profile-icon">Chat</a>
+  //          </li>
+  //          <li class="account-btn-sublist-item">
+  //            <a href="#" onclick="removeToken()"><img src="images/account-btn-logout.svg" loading="lazy" alt="" class="profile-icon">Log out</a>
+  //          </li>
+  //        </ul>
+  //      `
+  //     i.appendChild(accountBtnWrapper.cloneNode(true))
+  //   })
+  //   document.querySelector('.menu-btn-wrapper').appendChild(mobileMenuBtn)
+  // }
   
